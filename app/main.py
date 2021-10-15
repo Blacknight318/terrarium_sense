2b1b7892c004f8a8c56d30ee93cde5f19799cfb6
Dave Gaunky <Merlin615@gmail.com>
2021-10-14 23:18:53 -0500

    Added MQTT send and functional OTA

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import secrets
+from umqtt.robust import MQTTClient
+
 
 def read_ds_sensor():
     roms = ds_sensor.scan()
@@ -20,14 +22,30 @@ def wifi_connect():
         while not wifi.isconnected():
             pass
     print('Network ip info: ', wifi.ifconfig())
+    
+
+def send_to_mqtt_server(mqtt_client, mqtt_feed, temperature):
+    client = MQTTClient(client_id=mqtt_client, server=secrets.mqtt_server, user=secrets.mqtt_username, password=secrets.mqtt_key, ssl=False)
+    try:
+        client.connect()
+        client.publish(mqtt_feed, bytes(str(temperature), 'utf-8'), qos=0)
+        client.disconnect()
+    except Exception as e:
+        print('Failed to connected to MQTT Server with error: {}'.format(e))
+        pass
+
 
 wifi_connect()
+mqtt_client_id = bytes('client_'+str(int.from_bytes(os.urandom(3), 'little')), 'utf-8')
+mqtt_feedname = bytes('{:s}/feeds/{:s}'.format(secrets.mqtt_username, secrets.mqtt_temperature_topic), 'utf-8')
+
 while True:
     try:
         if gc.mem_free() < 102000:
             gc.collect()
         temp = read_ds_sensor()
         print(round(temp*(9/5)+32, 2))
+        send_to_mqtt_server(mqtt_client_id, mqtt_feedname, temp)
     except OSError as e:
         print(e)
     sleep(60)
