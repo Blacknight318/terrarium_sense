f86557816c1301f913705999d0b98ae83b4f8bac
Dave Gaunky <Merlin615@gmail.com>
2021-10-14 23:24:29 -0500

    Included sample secrets file, rename to secrets.py

---
+++ app/sample_secrets.py
@@ -0,0 +1,9 @@
+# Wireless info
+ssid = 'fbi'
+wifi_pass = 'password'
+
+# MQTT Info
+mqtt_server = 'io.adafruit.com'
+mqtt_username = 'blucher'
+mqtt_key = 'aio_00asdfg9807sdfgsdfg90809'
+mqtt_temperature_topic = 'temperature'
